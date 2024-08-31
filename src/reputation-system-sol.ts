a35978f6a2e9dc261a41788737557c834537e764
Emmanuel Ajao <emmanueladedoyin356@gmail.com>
2024-08-31 08:12:58 +0000

    first commit

--- src/reputation-system-sol.ts
+++ src/reputation-system-sol.ts
@@ -1,16 +1,21 @@
-import { ReputationUpdated as ReputationUpdatedEvent } from "../generated/ReputationSystem.sol/ReputationSystem.sol"
-import { ReputationUpdated } from "../generated/schema"
+import { ReputationUpdated as ReputationUpdatedEvent } from "../generated/ReputationSystem.sol/ReputationSystem_sol";
+import { ReputationUpdated } from "../generated/schema";
+import { Bytes } from "@graphprotocol/graph-ts";
 
 export function handleReputationUpdated(event: ReputationUpdatedEvent): void {
-  let entity = new ReputationUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.user = event.params.user
-  entity.newReputation = event.params.newReputation
+  let entityId = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let entity = ReputationUpdated.load(entityId);
 
-  entity.save()
+  if (!entity) {
+    entity = new ReputationUpdated(entityId);
+  }
+
+  entity.user = event.params.user;
+  entity.newReputation = event.params.newReputation;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
